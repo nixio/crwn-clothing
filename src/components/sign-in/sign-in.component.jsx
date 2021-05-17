5ee66fc228cb23be6aaf67b30ec0ddf0e6e73b2d
Nicol <nixio@users.noreply.github.com>
2021-05-16 20:25:51 -0400

    fixed labels

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -42,7 +42,7 @@ class SignIn extends React.Component {
             type="email"
             handleChange={this.handleChange}
             value={this.state.email}
-            label="email"
+            label="Email"
             required
           />
           <FormInput
@@ -50,7 +50,7 @@ class SignIn extends React.Component {
             type="password"
             value={this.state.password}
             handleChange={this.handleChange}
-            label="password"
+            label="Password"
             required
           />
           <div className="buttons">
